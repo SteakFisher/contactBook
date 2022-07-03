b7250513bdeab95f6123a0e2cb833d9ab223aa2b
SteakFisher <88953813+SteakFisher@users.noreply.github.com>
2022-07-03 16:10:28 +0400

    Reworked sign up and login system to include permLevel

--- main.py
+++ main.py
@@ -1,86 +1,85 @@
 from pprint import pprint
 import mysql.connector as sql
 
-db = sql.connect(username='root', password='00b', host='localhost', database='jay')
+db = sql.connect(user='root', password='00b', host='localhost', database='jay')
 cs = db.cursor()
 
+cs.execute(" CREATE TABLE IF NOT EXISTS logininfo(userId int primary key NOT NULL AUTO_INCREMENT," +
+           " username varchar(30) NOT NULL, password varchar(30) NOT NULL, permLevel varchar(20) DEFAULT 'user'" +
+           " NOT NULL, unique(username))")
+
+
 class User:
-    def __init__(self, userId, username, password):
+    def __init__(self, userId, username, password, permLevel):
         self.username = username
         self.password = password
         self.userId = userId
+        self.permLevel = permLevel
         # table, name = loginInfo with primary key = userId contains info such as user, pass and userId
-        # create table logininfo(userId int primary key NOT NULL AUTO_INCREMENT, username varchar(30) NOT NULL, password varchar(30) NOT NULL, unique(username));
+        # create table logininfo(userId int primary key NOT NULL AUTO_INCREMENT, username varchar(30) NOT NULL,
+        # password varchar(30) NOT NULL, unique(username));
 
-        # another table, name = contactInfo with foreign key = userId and primary key = contactId contains info such as userId, contactId, contactName, contactSurname, contactLastName
-        # create table contactInfo(userId int NOT NULL, contactId int primary key NOT NULL AUTO_INCREMENT, contactName varchar(30), contactSurname varchar(30),
+        # another table, name = contactInfo with foreign key = userId and primary key = contactId contains
+        # info such as userId, contactId, contactName, contactSurname, contactLastName
+        # create table contactInfo(userId int NOT NULL, contactId int primary key NOT NULL
+        # AUTO_INCREMENT, contactName varchar(30), contactSurname varchar(30),
         # contactLastName varchar(30), constraint userId foreign key(userId) references loginInfo(userid));
 
-        # another table, name = contactNumbers with Foreign Key = contactId and primary key = numId has more info such as contactNumber, contactType
-        # another table, name = events with Foreign Key = contactId and primary key = eventId has more info such as eventName, eventDate, eventLocation
+        # another table, name = contactNumbers with Foreign Key = contactId
+        # and primary key = numId has more info such as contactNumber, contactType
+        # another table, name = events with Foreign Key = contactId and
+        # primary key = eventId has more info such as eventName, eventDate, eventLocation
 
-
-
-    def getContacts(self):
-        cs.execute("SELECT * FROM contactInfo")
-        return cs.fetchall()
-
-def fetchUser(id):
-    try:
-        cs.execute("SELECT * FROM loginInfo WHERE userid = %s" %(id))
-    except:
-        return False
-    else:
-        t = cs.fetchall()
-        if(len(t) > 0):
-            return User(t[0][0], t[0][1], t[0][2])
-        else:
-            return False
 
 def login():
     username = input("Enter your username: ")
     password = input("Enter your password: ")
-    try:
-        cs.execute("SELECT userId FROM loginInfo WHERE username = '%s' AND password = '%s'" %(username, password))
-    except:
+    cs.execute(
+        "SELECT userId, permLevel FROM loginInfo WHERE username = '%s' AND password = '%s'" % (username, password))
+    t = cs.fetchall()
+    if len(t) > 0:
+        print("Login successful")
+        print(t[0][1])
+        return User(t[0][0], username, password, t[0][1])
+    else:
         print("Login failed")
         return False
-    else:
-        t = cs.fetchall()
-        if(len(t) > 0):
-            print("Login successful")
-            return User(t[0][0], username, password)
-        else:
-            print("Login failed")
-            return False
 
 
 def signUp():
     username = input("Enter your username: ")
-    try:
-        cs.execute("SELECT * FROM loginInfo WHERE username = '%s'" %(username))
-    except:
-        print("Sign up failed")
+    cs.execute("SELECT userid FROM loginInfo WHERE username = '%s'" % username)
+    t = cs.fetchall()
+    if len(t) > 0:
+        print("Username already exists")
         return False
     else:
+        password = input("Enter your password: ")
+        cs.execute("INSERT INTO loginInfo(username, password) VALUES('%s', '%s')" % (username, password))
+        db.commit()
+        print("Sign up successful")
+        cs.execute(
+            "SELECT userId, permLevel FROM loginInfo WHERE username = '%s' AND password = '%s'" % (username, password))
         t = cs.fetchall()
-        if (len(t) > 0):
-            print("Username already exists")
-            return False
-        else:
-            password = input("Enter your password: ")
-            cs.execute("INSERT INTO loginInfo(username, password) VALUES('%s', '%s')" % (username, password))
-            db.commit()
-            print("Sign up successful")
-            return False
+        return User(t[0][0], username, password, t[0][1])
+
 
 user = False
-while (user is False):
-    a = input("Login or Sign up? (l/s): ")
-    if (a.lower() == 'l'):
+while user is False:
+    print("""1) Login
+2) Sign up
+3) Exit
+(1/2/3)""")
+    a = input("")
+    if a == '1':
         user = login()
-    elif (a.lower() == 's'):
+    elif a == '2':
         user = signUp()
+    elif a == '3':
+        print("Goodbye")
+        break
     else:
-        print("Invalid input")
+        print("Invalid option, input 1,2 or 3!")
 
+pprint(vars(user))
+print("Welcome " + user.username + "!")
